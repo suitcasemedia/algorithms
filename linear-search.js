e86faaf4b4bb883fa5ca0f151067a6a94bd8c994
Jimmy Connelly <jimmyjconnelly@gmail.com>
2023-07-31 12:11:21 +0000

    change output string

--- linear-search.js
+++ linear-search.js
@@ -1,14 +1,15 @@
 const input = [2,3,4,5,8,9,10,12,15,17,18,19,20];
 const searchVal = 10;
 
-const linearSearch = (searchVal , input) =>{
-
-    for(let i = 0; i < input.length ; i++ ){
-
-        if(input[i] === searchVal){
-           console.log(`${i} is the position of the element in the array`)
+const linearSearch = (input, searchVal) => {
+ 
+    for(let i = 0 ; i < input.length ; i++){
+        if (input[i] === searchVal){
+            return `The index of ${searchVal} is ${i}`;
         }
     }
+    return `${searchVal} is not in the input array`
+
 }
 
-linearSearch( searchVal, input);
+console.log(linearSearch(input,searchVal));
